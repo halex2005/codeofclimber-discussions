46f3c94d4e1973986e6fdfeb792defb5a6f68c1e
halex2005 <halex2005@e1.ru>
2020-02-29 16:28:41 +0500

    fix importing *.module.scss files in components package

--- 2020-02-22-aspnetcore-with-monorepo-frontend-sample/frontend/packages/components/src/components/SomeView/index.tsx
+++ 2020-02-22-aspnetcore-with-monorepo-frontend-sample/frontend/packages/components/src/components/SomeView/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import styles from './SomeView.module.scss'
 
 export const SomeView: React.FC = () => (
-    <div>Some shared component!</div>
+    <div className={styles.root}>Some shared component!</div>
 )
